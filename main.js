d324f6325039eae4b0174c3ea5636d2d2d5eb78b
Lakia Jones <rachandra_jones@yahoo.com>
2019-03-02 11:08:01 -0600

    Updated JS

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 console.log('supercalifragilisticexpialidocious');
+// Lakia Jones
+// Kash Doll
+// City Girls
+// Tokyo Jetz
+// Cardi BBBB
+var firstName = 'Lakia';
+var lastName = 'Jones';
+console.log(firstName + ' '  + lastName)
+console.log(`${firstName} ${lastName}`)
+
+const firstName1 = 'Kash';
+const lastName1 = 'Doll';
+console.log(firstName1 + ' ' + lastName1)
+
+const firstName2 = 'City';
+const lastName2 = 'Girls';
+console.log(firstName2 + ' ' + lastName2)
+
+const namePrinter = (firstName, lastName) => {
+    console.log(`${firstName} ${lastName}`)
+    alert(`${firstName} ${lastName}`);
+}
+namePrinter('Tokyo', 'Jetz');
+namePrinter('Cardi', 'B!');
+namePrinter('Kash', 'Doll');
+
+const test = num => console.log(num);
+
+alert(firstName + lastName);
